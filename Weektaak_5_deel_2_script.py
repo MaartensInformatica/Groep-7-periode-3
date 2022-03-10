669d8f82b9af9161b0c1df3b835f049a8b4d7f8e
NickPolderHAN <n.vandepolder@student.han.nl>
2022-03-10 11:38:51 +0100

    - begonnen aan de histogram

--- Weektaak_5_deel_2_script.py
+++ Weektaak_5_deel_2_script.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as mpl
 
 
-def create_group_dict():
+def create_group_dict(step):
     """
     Creates a dictionary and adds a key for every percentage group.
     (0-2%, 2-4%, ...)
@@ -14,9 +14,9 @@ def create_group_dict():
 
     counter = 0
     while counter != 100:
-        group = str(counter) + "-" + str(counter + 2)
+        group = str(counter) + "-" + str(counter + step)
         percentage_groups[group] = 0
-        counter += 2
+        counter += step
 
     return percentage_groups
 
@@ -86,27 +86,42 @@ def add_percentages_to_dict(percentage_groups, value_count_dict):
 def create_histogram(percentage_groups):
     # keys
     x_as = []
-    #frequencies
+    # frequencies
     y_as = []
 
     for keys, values in percentage_groups.items():
         x_as.append(keys)
         y_as.append(values)
 
-    mpl.hist(y_as)
+    mpl.bar(x_as, y_as)
     mpl.xlabel("Percentage groups")
     mpl.ylabel("Frequency")
     mpl.tight_layout()
+    mpl.xlim(0, 24)
     mpl.show()
 
 
+def calculate_values(percentage_groups):
+    totaal = 0
+    gemiddelde = 0
 
+    value_counter = 0
+    for key in percentage_groups:
+        value = percentage_groups[key]
+        totaal += value
+        value_counter += 1
+
+    gemiddelde = totaal / value_counter
+    print(gemiddelde)
 
 if __name__ == '__main__':
     test_file = "44 test.txt"
-    percentage_groups = create_group_dict()
+    # bepaalt de grootte van de percentage groups.
+    percentage_group_step = 2
+    percentage_groups = create_group_dict(percentage_group_step)
     percentage_list = append_identity_percentages(test_file)
     value_count_dict = count_percentages(percentage_list)
     percentage_groups = add_percentages_to_dict(percentage_groups,
                                                 value_count_dict)
     create_histogram(percentage_groups)
+    calculate_values(percentage_groups)
