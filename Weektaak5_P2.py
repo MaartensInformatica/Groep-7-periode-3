811945b8fb83cf32be192a3874af39822f933b81
RemkeWagenaar <90325505+RemkeWagenaar@users.noreply.github.com>
2022-03-11 10:35:17 +0100

    Update Weektaak5_P2.py

--- Weektaak5_P2.py
+++ Weektaak5_P2.py
@@ -1,18 +1,27 @@
+import numpy
 import matplotlib.pyplot as plt
 
 
-def append_identity_percentages(percentage_file):
+def read_file(file_name):
     """
-    :param percentage_file:
-    :return:
     """
 
     lines = []
-    with open(percentage_file, "r") as file:
+    with open(file_name, "r") as file:
         for line in file:
             line = line.strip()
             if "#" not in line and line != "":
                 lines.append(line)
+
+    file.close()
+
+    return lines
+
+
+def get_percentages(lines):
+    """
+
+    """
 
     list_percentages = []
     for line in lines:
@@ -50,11 +59,35 @@ def count_percentages(list_percentages):
     return list_values
 
 
-def create_histogram(list_values):
+def calculate_mean(list_values):
+    """"
+    """
+
+    mean_percentage = numpy.mean(list_values)
+
+    return mean_percentage
+
+
+def calculate_standard_deviation(list_values):
+    """"
+    """
+
+    stdev_percentage = numpy.std(list_values)
+
+    return stdev_percentage
+
+
+def create_histogram(list_values, mean_percentage, stdev_percentage,
+                     mean_blast):
     """"
     """
 
     plt.hist(list_values, density=True, bins=300, color='green')
+    plt.axvline(mean_percentage, color='r', linewidth=1)
+    plt.axvline(mean_blast, color='yellow', linewidth=1)
+
+    plt.axvline(mean_percentage - stdev_percentage, color='b', linewidth=1)
+    plt.axvline(mean_percentage + stdev_percentage, color='b', linewidth=1)
 
     plt.xlabel("Percentages (%)")
     plt.ylabel("Fraction")
@@ -64,7 +97,13 @@ def create_histogram(list_values):
 
 
 if __name__ == '__main__':
-    test_file = "results clustalO.txt"
-    list_percentages = append_identity_percentages(test_file)
+    identities_blast = [34, 36, 31, 45, 34, 34]
+    file_name = "results clustalO.txt"
+    lines = read_file(file_name)
+    list_percentages = get_percentages(lines)
     list_values = count_percentages(list_percentages)
-    create_histogram(list_values)
+    mean_percentage = calculate_mean(list_values)
+    stdev_percentage = calculate_standard_deviation(list_values)
+    mean_blast = calculate_mean(identities_blast)
+    create_histogram(list_values, mean_percentage, stdev_percentage,
+                     mean_blast)
